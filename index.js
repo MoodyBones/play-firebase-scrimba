0e7cece76ad994eda4a178d1dec7532f0a4ac2f2
Mel <jones.mel@gmail.com>
2025-08-29 19:44:27 +1000

    add signInWithEmailAndPassword function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* === Imports === */
 import { initializeApp } from "firebase/app"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "firebase/auth"
 
 
 /* === Firebase Setup === */
@@ -48,7 +52,30 @@ function authSignInWithGoogle() {
 }
 
 function authSignInWithEmail() {
-    console.log("Sign in with email and password")
+    /*  Challenge:
+           Import the signInWithEmailAndPassword function from 'firebase/auth'
+   
+           Use the code from the documentaion to make this function work.
+           
+           Make sure to first create two consts, 'email' and 'password', to fetch the values from the input fields emailInputEl and passwordInputEl.
+          
+           If the login is successful then you should show the logged in view using showLoggedInView()
+           If something went wrong, then you should log the error message using console.error.
+       */
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user
+            showLoggedInView()
+        })
+        .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+            console.error(errorMessage)
+        })
 }
 
 function authCreateAccountWithEmail() {
